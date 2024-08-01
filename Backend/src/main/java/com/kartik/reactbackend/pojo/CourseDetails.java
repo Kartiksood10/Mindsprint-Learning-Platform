483f5ca7a666ecd2814690b57294415bd4b2a12d
Kartiksood10 <kartiksood2002@gmail.com>
2024-08-01 10:03:21 +0530

    initial commit

---
+++ Backend/src/main/java/com/kartik/reactbackend/pojo/CourseDetails.java
@@ -0,0 +1,59 @@
+package com.swapnil.reactbackend.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CourseDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private int id;
+	
+	private String coursename;
+	
+	private String courseauthor;
+	
+	private String url;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCoursename() {
+		return coursename;
+	}
+
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+
+	public String getCourseauthor() {
+		return courseauthor;
+	}
+
+	public void setCourseauthor(String courseauthor) {
+		this.courseauthor = courseauthor;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ Backend/src/main/java/com/kartik/reactbackend/pojo/User.java
@@ -0,0 +1,58 @@
+package com.swapnil.reactbackend.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String username;
+	
+	private String userid;
+	
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
